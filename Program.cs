647301ff27336276cb18b8249fc2043eb58a72f7
artsingletary <artsingletary@yahoo.com>
2017-11-05 14:20:44 -0500

    added initial code for first program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Systems.Text;
+using System.Threading.Tasks;
+
+
+namespace Dotnet
+class Program
+{
+  static void Main(string[] args)
+  {
+     string name;
+     Console.WriteLine ("Hello World!");
+  
+  }
+
+  }
+}
