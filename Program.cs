94cca771d9fc4f5a0aced36d1aebf6863b196a96
artsingletary <artsingletary@yahoo.com>
2017-11-06 15:14:18 -0500

    Added some variables

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 23.14f;
+            char character = 'A';
+            string firstName = "Art";
+            bool isWorking = false;
+
+
+            Console.WriteLine(number);
+            System.Console.WriteLine(count);
+            System.Console.WriteLine(totalPrice);
+            System.Console.WriteLine(character);
+            System.Console.WriteLine(firstName);
+            System.Console.WriteLine(isWorking);
+
+            Console.Write("What is your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Random rand = new Random();
+            Console.WriteLine("Random Number between 1 and 10 " + (rand.Next(1,11)));
         }
     }
 }
